5f3e6138f574352110b7886c78e6bb00798f7c28
intellimath1 <clintonenwerem@gmail.com>
2020-08-31 17:10:22 +0100

    Fixed the issue of constant distance measurements and added control for a second motor

--- src/hello_world/scripts/motor_driver.py
+++ src/hello_world/scripts/motor_driver.py
@@ -14,28 +14,36 @@ driverDetails = motorDriver()
 
 driverDetails.ENABLE_A = 8
 driverDetails.ENABLE_B = 4
-driverDetails.dir1 = 5
-driverDetails.dir2 = 6
-driverDetails.dir3 = 24
-driverDetails.dir4 = 25
+driverDetails.dir1 = 13
+driverDetails.dir2 = 19
+driverDetails.dir3 = 20
+driverDetails.dir4 = 16
 
 #GPIO.setup(driverDetails.ENABLE_B, GPIO.OUT)
+GPIO.setup(driverDetails.dir1, GPIO.OUT)
+GPIO.setup(driverDetails.dir2, GPIO.OUT)
 GPIO.setup(driverDetails.dir3, GPIO.OUT)
 GPIO.setup(driverDetails.dir4, GPIO.OUT)
 
 #GPIO.output(driverDetails.ENABLE_B, True)
 
 def forward():
+    GPIO.output(driverDetails.dir1, True)
+    GPIO.output(driverDetails.dir2, False)
     GPIO.output(driverDetails.dir3, True)
     GPIO.output(driverDetails.dir4, False)
 
 
 def backward():
+    GPIO.output(driverDetails.dir1, False)
+    GPIO.output(driverDetails.dir2, True)
     GPIO.output(driverDetails.dir3, False)
     GPIO.output(driverDetails.dir4, True)
 
 
 def stop():
+    GPIO.output(driverDetails.dir1, False)
+    GPIO.output(driverDetails.dir2, False)
     GPIO.output(driverDetails.dir3, False)
     GPIO.output(driverDetails.dir4, False)
 
@@ -63,4 +71,10 @@ def receiveDistance():
 
 if __name__ == "__main__":
 
-    receiveDistance()
+    try:
+
+        receiveDistance()
+
+    except rospy.ROSInterruptException:
+
+        stop()
--- src/hello_world/scripts/ultrasonic_sensor.py
+++ src/hello_world/scripts/ultrasonic_sensor.py
@@ -18,41 +18,38 @@ GPIO.setup(sensorDetails.TRIG, GPIO.OUT)
 GPIO.setup(sensorDetails.ECHO, GPIO.IN)
 
 def calcDistance():
-    pub = rospy.Publisher('distance', ultRangerData, queue_size=10)
-    rospy.init_node('sensor_node', anonymous=True)
-    rate = rospy.Rate(0.5)
 
-    GPIO.output(sensorDetails.TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(sensorDetails.TRIG, False)
+	while not rospy.is_shutdown():
+		pub = rospy.Publisher('distance', ultRangerData, queue_size=10)
+        	rospy.init_node('sensor_node', anonymous=True)
+		rate = rospy.Rate(2)
 
-    pulse_start = time.time()
+    		GPIO.output(sensorDetails.TRIG, True)
+    		time.sleep(0.00001)
+    		GPIO.output(sensorDetails.TRIG, False)
 
-    while GPIO.input(sensorDetails.ECHO) == 0:
-        pulse_start = time.time()
+    		pulse_start = time.time()
 
-    while GPIO.input(sensorDetails.ECHO) == 1:
-        pulse_end = time.time()
+    		while GPIO.input(sensorDetails.ECHO) == 0:
+        		pulse_start = time.time()
 
-    pulse_duration = pulse_end - pulse_start
+    		while GPIO.input(sensorDetails.ECHO) == 1:
+        		pulse_end = time.time()
 
-    while not rospy.is_shutdown():
-
-        dist = pulse_duration * 17150
-        sensorDetails.distance=dist
-	time.sleep(2)
-
-        success_str = "Distance recorded and published: %f" %dist
-        rospy.loginfo(success_str)
-        pub.publish(sensorDetails)
-        rate.sleep()
+    		pulse_duration = pulse_end - pulse_start
+    		dist = pulse_duration * 17150
+    		sensorDetails.distance=dist
+    		success_str = "Distance recorded and published: %f" %dist
+    		rospy.loginfo(success_str)
+    		pub.publish(sensorDetails)
+    		rate.sleep()
 
 
 if __name__ == "__main__":
-    try:
-        while True:
-            calcDistance()
 
-    except rospy.ROSInterruptException:
-        GPIO.cleanup()
-        pass
+	try:
+		calcDistance()
+
+	except rospy.ROSInterruptException:
+
+		pass
